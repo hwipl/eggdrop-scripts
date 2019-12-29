820c7da86ea9105765afc5f56cb9c7ecf6a1145e
hwipl <33433250+hwipl@users.noreply.github.com>
2019-12-30 00:13:00 +0100

    update date.tcl
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- scripts/date.tcl
+++ scripts/date.tcl
@@ -1,8 +1,37 @@
 # date.tcl
+#
+# This script shows the current date and time with the !date command.
+#
+# Usage:
+#       !date                   show the current date and time
+#
+# Enable for a channel with:    .chanset #channel +date
+# Disable for a channel with:   .chanset #channel -date
 
-proc show_date { nick host hand chan arg } {
-	set date_format "%a %b %d %H:%M:%S %Z %Y"
-	puthelp "PRIVMSG $chan :[clock format [clock seconds] -format $date_format]"
+# tested versions, might run on earlier versions
+package require Tcl 8.6
+package require eggdrop 1.8.4
+
+namespace eval ::date {
+	# channel flag for enabling/disabling
+	setudef flag date
 }
 
-bind pub - !date show_date
+proc ::date::show_date { nick host hand chan arg } {
+	# check channel flag if enabled in this channel
+	if {![channel get $chan date]} {
+		return 0
+	}
+
+	# get current date
+	set date_format "%a %b %d %H:%M:%S %Z %Y"
+	set date [clock format [clock seconds] -format $date_format]
+
+	puthelp "PRIVMSG $chan :$date"
+	return 1
+}
+
+namespace eval ::date {
+	bind pub - !date ::date::show_date
+	putlog "Loaded date.tcl"
+}
